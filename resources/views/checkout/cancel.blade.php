496af21d70aa3339d299ba8541cc61761f2224ec
Matheus <matheusnascimentosilva574@gmail.com>
2025-05-22 18:59:22 -0300

    feat(checkout): add payment success and cancellation views

---
+++ resources/views/checkout/cancel.blade.php
@@ -0,0 +1,3 @@
+<h1>Pagamento cancelado</h1>
+<p>Seu pagamento não foi concluído.</p>
+<a href="/">Voltar para a loja</a>
---
+++ resources/views/checkout/success.blade.php
@@ -0,0 +1,3 @@
+<h1>Pagamento concluído com sucesso!</h1>
+<p>Obrigado por comprar: {{ $product->name }}</p>
+<a href="/">Voltar para a loja</a>
